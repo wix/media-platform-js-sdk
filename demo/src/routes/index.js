c81b11a245661ff6eaca7a23dead0d8c106da870
elad laufer <elad@wix.com>
2017-03-07 18:01:09 +0200

    - started working on server side demo

--- demo/src/routes/index.js
+++ demo/src/routes/index.js
@@ -1,5 +1,9 @@
 module.exports = function(app) {
     app.get('/', function(req, res, next) {
-        res.render('index', { title: 'Wix Media Platform - Demo' });
+        res.render('browser', { title: 'Wix Media Platform - Browser Demo' });
+    });
+
+    app.get('/server', function(req, res, next) {
+        res.render('server', { title: 'Wix Media Platform - Server Demo' });
     });
 };
