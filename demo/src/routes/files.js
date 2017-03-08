dcd30c86cfe9ce97affbb0f08de000c3be676143
elad laufer <elad@wix.com>
2017-03-08 16:25:43 +0200

    - better styles - paginate list

--- demo/src/routes/files.js
+++ demo/src/routes/files.js
@@ -1,11 +1,39 @@
 var mediaPlatform = require('../facades/media-platform-facade').mediaPlatform;
+var ListFilesRequest = require('../../../src/index').file.ListFilesRequest;
 
 var fileManager = mediaPlatform.fileManager;
 
 module.exports = function(app) {
 
+    app.get('/media-platform/file/upload/url', function(req, res) {
+        fileManager.getUploadUrl(null, function (error, uploadCredentials) {
+
+            if (error) {
+                res.status(500).send(error.message);
+                return;
+            }
+
+            res.send(uploadCredentials);
+        });
+    });
+
+    app.get('/media-platform/file/upload', function(req, res) {
+        var rand = Math.floor((Math.random() * 100000) + 1);
+        fileManager.uploadFile('/demo/' + rand + '.image.jpg', __dirname + '/../files/image.jpg', null, function (error, response) {
+
+            if (error) {
+                res.status(500).send(error.message);
+                return;
+            }
+
+            res.send(response);
+        });
+
+    });
+
     app.get('/media-platform/files', function(req, res) {
-        fileManager.listFiles('/demo', null, function (error, listFilesResponse) {
+        var listFilesRequest = new ListFilesRequest().setPageSize(3);
+        fileManager.listFiles('/demo', listFilesRequest, function (error, listFilesResponse) {
 
             if (error) {
                 res.status(500).send(error.message);
@@ -25,32 +53,6 @@ module.exports = function(app) {
             }
 
             res.send(response);
-        });
-    });
-
-    app.get('/media-platform/file/upload', function(req, res) {
-        var rand = Math.floor((Math.random() * 100000) + 1);
-        fileManager.uploadFile('/demo/' + rand + '.image.jpg', __dirname + '/../files/image.jpg', null, function (error, response) {
-
-            if (error) {
-                res.status(500).send(error.message);
-                return;
-            }
-
-            res.send(response);
-        });
-
-    });
-
-    app.get('/media-platform/file/upload/url', function(req, res) {
-        fileManager.getUploadUrl(null, function (error, uploadCredentials) {
-
-            if (error) {
-                res.status(500).send(error.message);
-                return;
-            }
-
-            res.send(uploadCredentials);
         });
     });
 
