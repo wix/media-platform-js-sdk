6ce8e36b1d163fcbe6f9edc06e82d3aaf0667f70
Svyatoslav Shmatko <svyatoslav@wix.com>
2017-03-08 17:07:08 +0200

    add highlight.js

--- demo/app.js
+++ demo/app.js
@@ -13,7 +13,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 app.use(express.static(path.join(__dirname, '/../dist')));
-
+app.use(express.static(path.join(__dirname, '/../node_modules/highlight/lib')));
 /**
  * replace with your config
  */
