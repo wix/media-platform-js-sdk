e4bb5caad2bcb965d870bdf502a173039f87962f
elad laufer <elad@wix.com>
2017-03-09 17:25:06 +0200

    - bah...

--- public/platform/uploader/browser-file-uploader.js
+++ public/platform/uploader/browser-file-uploader.js
@@ -39,11 +39,9 @@ FileUploader.prototype.getUploadUrl = function (mediaType, callback) {
 * @description upload a file
 * @param {string} path the destination to which the file will be uploaded
 * @param {File} file
-* @param {UploadFileRequest?} uploadRequest
-* @param {function(Error, Array<FileDescriptor>|null)} callback
+* @returns {UploadJob}
 */
-//FileUploader.prototype.uploadFile = function (path, file, uploadRequest, callback) {
-FileUploader.prototype.uploadFile = function (path, file, uploadRequest, callback) {
+FileUploader.prototype.uploadFile = function (path, file) {
     var uploadJob = new UploadJob(path, file);
 
     return uploadJob.run(this);
