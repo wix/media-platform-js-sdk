48882a3bbd7cde5e8bc7301a70da21438ca2462f
Svyatoslav Shmatko <svyatoslav@wix.com>
2017-03-09 19:41:38 +0200

    - use prism.js for code highlighting

--- demo/app.js
+++ demo/app.js
@@ -13,7 +13,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 app.use(express.static(path.join(__dirname, '/../dist')));
-app.use(express.static(path.join(__dirname, '/../node_modules/highlight/lib')));
+app.use(express.static(path.join(__dirname, '../node_modules')));
 app.use(express.static(path.join(__dirname, '/src/client')));
 
 /**
