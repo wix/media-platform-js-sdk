1ba3ca4a708d9ef52f576c4bfd459ed542c1fa6c
elad laufer <elad@wix.com>
2017-03-09 18:36:15 +0200

    - added limited token example

--- demo/src/routes/authentication.js
+++ demo/src/routes/authentication.js
@@ -1,4 +1,6 @@
 var mediaPlatform = require('../facades/media-platform-facade').mediaPlatform;
+var Token = require('../../../src/index').auth.Token;
+var NS = require('../../../src/index').auth.NS;
 
 module.exports = function(app) {
 
@@ -7,10 +9,29 @@ module.exports = function(app) {
      */
     app.get('/media-platform/auth-header', function (req, res, next) {
         /**
-         * @description by default, the header authenticates the application
+         * @description by default, the header authorizes all the application verbs
          * @type {{Authorization}}
          */
         var header = mediaPlatform.getAuthorizationHeader();
+
+        res.send(header);
+    });
+
+    /**
+     * Your own authentication mechanism comes here
+     */
+    app.get('/media-platform/limited-auth-header', function (req, res, next) {
+
+        var token = new Token()
+            .setIssuer(NS.APPLICATION, '48fa9aa3e9d342a3a33e66af08cd7fe3')
+            .setSubject(NS.APPLICATION, '48fa9aa3e9d342a3a33e66af08cd7fe3')
+            .addVerbs('urn:service:file.get', 'urn:service:file.list');
+
+        /**
+         * @description you can limit the token to certain verbs
+         * @type {{Authorization}}
+         */
+        var header = mediaPlatform.getAuthorizationHeader(token);
 
         res.send(header);
     });
