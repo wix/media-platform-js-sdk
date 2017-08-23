dd8a2708cceb5b08c84c681bdf298d6943c7e361
elad laufer <elad@wix.com>
2017-08-23 15:13:36 +0300

    - added verbs

--- src/platform/authentication/VERB.js
+++ src/platform/authentication/VERB.js
@@ -2,19 +2,31 @@ var NS = require('./NS');
 
 /**
  * @type {{
- *     FILE_UPLOAD: string,
- *     FILE_CREATE: string,
- *     FILE_GET: string,
- *     FILE_LIST: string,
- *     FILE_DOWNLOAD: string,
- *     FILE_DELETE: string
+ * FILE_UPLOAD: string,
+ * FILE_IMPORT: string,
+ * FILE_CREATE: string,
+ * FILE_GET: string,
+ * FILE_LIST: string,
+ * FILE_DOWNLOAD: string,
+ * FILE_DELETE: string,
+ * AV_TRANSCODE: string,
+ * AV_REPACKAGE: string,
+ * ARCHIVE_CREATE: string,
+ * ARCHIVE_EXTRACT: string
  * }}
  */
 module.exports = {
     FILE_UPLOAD: NS.SERVICE + 'file.upload',
+    FILE_IMPORT: NS.SERVICE + 'file.import',
     FILE_CREATE: NS.SERVICE + 'file.create',
     FILE_GET: NS.SERVICE + 'file.get',
     FILE_LIST: NS.SERVICE + 'file.list',
     FILE_DOWNLOAD: NS.SERVICE + 'file.download',
-    FILE_DELETE: NS.SERVICE + 'file.delete'
+    FILE_DELETE: NS.SERVICE + 'file.delete',
+
+    AV_TRANSCODE: NS.SERVICE + 'av.transcode',
+    AV_REPACKAGE: NS.SERVICE + 'av.repackage',
+
+    ARCHIVE_CREATE: NS.SERVICE + 'archive.create',
+    ARCHIVE_EXTRACT: NS.SERVICE + 'archive.extract'
 };
