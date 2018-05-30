0664da81f9d69779d9200b1ec826cab607306944
bildja <bildja2@gmail.com>
2018-05-30 13:40:54 +0300

    Updates

--- path----d710d5a0c866ca257095.js
+++ path----d710d5a0c866ca257095.js
@@ -1,2 +1,2 @@
-webpackJsonp([60335399758886],{109:function(t,e){t.exports={data:{site:{siteMetadata:{title:"Media Platform JS SDK",description:"",keywords:""}}},layoutContext:{}}}});
+webpackJsonp([60335399758886],{110:function(t,e){t.exports={data:{site:{siteMetadata:{title:"Media Platform JS SDK",description:"",keywords:""}}},layoutContext:{}}}});
 //# sourceMappingURL=path----d710d5a0c866ca257095.js.map
