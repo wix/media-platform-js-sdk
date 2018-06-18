4b639f982fd6c2d55e62c9b8cf27cf2c87c442ff
Leon Gordin <lelikg@gmail.com>
2018-06-18 16:04:22 +0300

    live stream list response + tests fix (#34)

---
+++ src/platform/management/responses/live-stream-list-response.ts
@@ -0,0 +1,34 @@
+import {ILiveStream, LiveStream} from '../metadata/live-stream';
+
+/**
+ * Live Stream List Response interface
+ * @doc LiveStreamListResponse
+ */
+export interface ILiveStreamListResponse {
+  nextPageToken: string;
+  streams: ILiveStream[];
+}
+
+/**
+ * Search Live Stream List Response
+ * @doc LiveStreamListResponse
+ */
+export class LiveStreamListResponse {
+  public nextPageToken: string | undefined = undefined;
+  public streams: LiveStream[] | undefined = [];
+
+  constructor(data: ILiveStreamListResponse) {
+    this.deserialize(data);
+  }
+
+  /**
+   * @param data
+   * @private
+   */
+  deserialize(data: ILiveStreamListResponse) {
+    this.nextPageToken = data.nextPageToken;
+    this.streams = data.streams.map((liveStream) => {
+      return new LiveStream(liveStream);
+    });
+  }
+}
