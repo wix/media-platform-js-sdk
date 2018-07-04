bd4bb7da5ecc564b9bea49add4e61764dcb08f48
Dima Bildin <bildja2@gmail.com>
2018-07-04 17:24:44 +0300

    Revert "Create media platform factory"
    This reverts commit 179f72a46b02e5a8e1d4a62970d9c6de98a0b714.

--- src/main.ts
+++ src/main.ts
@@ -25,10 +25,8 @@ import {VERB} from './platform/authentication/VERB';
 import {ExtractStoryboardSpecification} from './platform/management/job/extract-storyboard-specification';
 import {ExtractPosterSpecification} from './platform/management/job/extract-poster-specification';
 import {LiveStreamListRequest} from "./platform/management/requests/live-stream-list-request";
-import {createMediaPlatform} from './media-platform-factory';
 
 export {
-  createMediaPlatform,
   /**
    * @type {MediaPlatform}
    */
--- src/public/platform/configuration/configuration.ts
+++ src/public/platform/configuration/configuration.ts
@@ -1,14 +1,10 @@
 import {IConfigurationBase} from '../../../platform/configuration/configuration';
 
-export interface IConfigurationBrowser extends IConfigurationBase {
-  authenticationUrl: string;
-}
-
 /**
  * browser configuration
  * @doc Configuration
  */
-export class Configuration implements IConfigurationBrowser {
+export class Configuration implements IConfigurationBase {
   constructor(public domain: string, public authenticationUrl: string) {
   }
 }
