cf4cdf321c64c8df397f016caac6524874c8ac4c
Aleksandr Davydov <aleksandrd@wix.com>
2018-11-07 16:37:12 +0200

    Updates

--- path----d710d5a0c866ca257095.js
+++ path----d710d5a0c866ca257095.js
@@ -1,2 +1,2 @@
-webpackJsonp([60335399758886],{124:function(t,e){t.exports={data:{site:{siteMetadata:{title:"Media Platform JS SDK",description:"",keywords:""}}},layoutContext:{}}}});
+webpackJsonp([60335399758886],{123:function(t,e){t.exports={data:{site:{siteMetadata:{title:"Media Platform JS SDK",description:"",keywords:""}}},layoutContext:{}}}});
 //# sourceMappingURL=path----d710d5a0c866ca257095.js.map
