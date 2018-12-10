543e496e3d80551ada91b37fd51ec3cccc29292d
lelikg <lelikg@gmail.com>
2018-12-10 16:35:02 +0200

    add test for watermark manifest + serialize

---
+++ test/server/image/filter/watermark.spec.ts
@@ -0,0 +1,37 @@
+import {expect} from 'chai';
+import {Image} from '../../../../src';
+import {Watermark} from '../../../../src/image/filter/watermark';
+
+describe('watermark', () => {
+
+  it('serializes', () => {
+    const watermark = new Watermark(new Image());
+    watermark.manifest('manifest');
+
+    expect(watermark.serialize()).to.deep.equal({params: 'wm_manifest', error: null});
+  });
+
+  it('resets for null', () => {
+    const watermark = new Watermark(new Image());
+    watermark.manifest('manifest');
+    watermark.manifest();
+
+    expect(watermark.serialize()).to.deep.equal({params: '', error: null});
+  });
+
+  it('resets for undefined', () => {
+    const watermark = new Watermark(new Image());
+    watermark.manifest('manifest');
+    watermark.manifest(undefined);
+
+    expect(watermark.serialize()).to.deep.equal({params: '', error: null});
+  });
+
+  it('resets for empty string', () => {
+    const watermark = new Watermark(new Image());
+    watermark.manifest('manifest');
+    watermark.manifest('');
+
+    expect(watermark.serialize()).to.deep.equal({params: '', error: null});
+  });
+});
