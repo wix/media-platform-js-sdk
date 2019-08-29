b2b6a399fd601068457de6c1a27e630077ff00af
lelikg <lelikg@gmail.com>
2019-08-29 16:27:54 +0300

    feat: more domain object typings for flow control fix: some flow adjustments chore: e2e test with demo account

--- src/platform/management/metadata/flow-component.ts
+++ src/platform/management/metadata/flow-component.ts
@@ -1,16 +1,24 @@
-import { Successor } from '../../../types/media-platform/media-platform';
+import { Callback, ICallback } from '../job/callback';
+import { FileDescriptor, IFileDescriptor } from './file-descriptor';
+import { ISource, Source } from '../job/source';
 
 export interface IFlowComponent {
   type: string;
   specification: any;
-  successors?: Successor[];
+  callback?: ICallback;
+  results?: IFileDescriptor[];
+  sources?: ISource[];
+  successors?: string[];
   status?: string | null;
 }
 
 export class FlowComponent implements IFlowComponent {
   public type: string;
   public specification: any | null = null;
-  public successors: Successor[] = [];
+  public callback?: ICallback;
+  public results: FileDescriptor[] = [];
+  public sources: Source[] = [];
+  public successors: string[] = [];
   public status: string | null = null;
 
   constructor(data: IFlowComponent) {
@@ -19,6 +27,20 @@ export class FlowComponent implements IFlowComponent {
 
     if (data.successors) {
       this.successors = data.successors;
+    }
+
+    if (data.callback) {
+      this.callback = new Callback(data.callback);
+    }
+
+    if (data.sources) {
+      this.sources = data.sources.map(source => new Source(source));
+    }
+
+    if (data.results) {
+      this.results = data.results.map(
+        fileDescriptor => new FileDescriptor(fileDescriptor),
+      );
     }
 
     this.status = data.status ? data.status : null;
