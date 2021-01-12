4c4bfc849e11e46d717d8110c625d668f2889dad
lelikg <lelikg@gmail.com>
2021-01-12 18:11:59 +0200

    update e2e credentials remove husky

--- test/server/credentials.ts
+++ test/server/credentials.ts
@@ -2,9 +2,9 @@
 
 import { Configuration } from '../../src/platform/configuration/configuration';
 
-const DEMO_APP_DOMAIN = 'wixmp-4b1aa7b28bada0ca23c4d299.appspot.com';
-const DEMO_APP_APP_ID = '959010acd669459c807ad9e1986a25e8';
-const DEMO_APP_SHARED_SECRET = 'dcc04e5f2ca89d95ef488fa5b3adf736';
+const DEMO_APP_DOMAIN = 'wixmp-6d17b5efd6f94144cded4052.appspot.com';
+const DEMO_APP_APP_ID = '624d51e89a7a4c4b95f8b04cd5780c24';
+const DEMO_APP_SHARED_SECRET = '661633b875414aee0fc50513b87c45f6';
 
 export const credentials: Configuration = {
   domain: process.env.WIX_TEST_DOMAIN || DEMO_APP_DOMAIN,
